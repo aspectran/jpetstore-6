d867f423e809a0f215f81e02ced16a235c6584df
Juho Jeong <aspectran@gmail.com>
2019-02-28 00:07:56 +0900

    Update to latest aspectran

--- src/main/java/org/mybatis/jpetstore/common/mybatis/ReuseSqlSession.java
+++ src/main/java/org/mybatis/jpetstore/common/mybatis/ReuseSqlSession.java
@@ -3,7 +3,7 @@ package org.mybatis.jpetstore.common.mybatis;
 import com.aspectran.core.component.bean.annotation.AvoidAdvice;
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
-import com.aspectran.with.mybatis.SqlSessionAgent;
+import com.aspectran.mybatis.SqlSessionAgent;
 
 @Component
 @Bean("reuseSqlSession")
