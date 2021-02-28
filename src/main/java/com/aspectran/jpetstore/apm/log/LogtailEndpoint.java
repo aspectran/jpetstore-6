8b3a2ad09578599ae1a57394ca0c50bc9f45006e
Juho Jeong <aspectran@gmail.com>
2021-02-28 23:42:17 +0900

    Update

--- src/main/java/com/aspectran/jpetstore/apm/log/LogtailEndpoint.java
+++ src/main/java/com/aspectran/jpetstore/apm/log/LogtailEndpoint.java
@@ -22,7 +22,6 @@ import com.aspectran.core.util.StringUtils;
 import com.aspectran.core.util.logging.Logger;
 import com.aspectran.core.util.logging.LoggerFactory;
 import com.aspectran.websocket.jsr356.AspectranConfigurator;
-
 import jakarta.websocket.CloseReason;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
@@ -30,6 +29,7 @@ import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Collections;
